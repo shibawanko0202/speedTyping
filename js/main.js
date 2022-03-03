fd90f0556a518192ff490af86b72b455c9222921
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2022-03-04 01:09:23 +0900

    アニメーションの追加

--- js/main.js
+++ js/main.js
@@ -168,6 +168,11 @@
       scoreCount++;
       score.textContent = scoreCount;
       rate();
+      //文字を跳ねさせる
+      score.classList.add("pyon");
+      score.addEventListener("animationend",()=>{
+        score.classList.remove("pyon");
+      });
 
       //タイプ音を鳴らす
       typeSound.currentTime = 0;
@@ -192,6 +197,11 @@
       badCount++;
       bad.textContent = badCount;
       rate();
+      //文字を跳ねさせる
+      bad.classList.add("pyon");
+      bad.addEventListener("animationend",()=>{
+        bad.classList.remove("pyon");
+      });
 
       //ブザーを鳴らす
       badSound.currentTime = 0;
